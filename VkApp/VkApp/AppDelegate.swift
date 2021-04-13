b34c638fd65ccf358eb7af59caf5bec7392df494
KirillovAlexander <kirillov_sasha98@mail.ru>
2021-04-13 23:35:44 +0300

    Create project

---
+++ VkApp/VkApp/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  VkApp
+//
+//  Created by Macbook on 13.04.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
