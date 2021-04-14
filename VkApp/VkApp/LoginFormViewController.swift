c57de9b99a65e027c2ba127bd8fc5aecfb623d23
KirillovAlexander <kirillov_sasha98@mail.ru>
2021-04-14 16:30:27 +0300

    Make Login Form ViewController

---
+++ VkApp/VkApp/LoginFormViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormViewController.swift
+//  VkApp
+//
+//  Created by Macbook on 14.04.2021.
+//
+
+import UIKit
+
+class LoginFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
