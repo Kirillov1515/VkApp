3fdab881cb78f4ce26fde8a0f327cfd0669f41aa
KirillovAlexander <kirillov_sasha98@mail.ru>
2021-04-17 00:46:44 +0300

    Add keyboard notification

--- VkApp/VkApp/LoginFormViewController.swift
+++ VkApp/VkApp/LoginFormViewController.swift
@@ -13,12 +13,29 @@ class LoginFormViewController: UIViewController {
     
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     
     //MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     //MARK: - Sign in implementation
@@ -27,7 +44,7 @@ class LoginFormViewController: UIViewController {
         if checkUserData() {
             performSegue(withIdentifier: signInSegue, sender: self)
         } else {
-            showAlertWindow()
+            showSignInErrorAlert()
         }
     }
     
@@ -40,10 +57,29 @@ class LoginFormViewController: UIViewController {
         return false
     }
     
-    func showAlertWindow() {
+    func showSignInErrorAlert() {
         let alert = UIAlertController(title: "Error", message: "Wrong user name or password", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
         present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Keyboard functions
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let keyboardSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
     }
     
     //MARK: - Exit to this controller action
