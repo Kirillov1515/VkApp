341be0c40348432649a3d76c69455beee65248a5
KirillovAlexander <kirillov_sasha98@mail.ru>
2021-04-16 17:24:18 +0300

    Add sign in implementation

--- VkApp/VkApp/LoginFormViewController.swift
+++ VkApp/VkApp/LoginFormViewController.swift
@@ -9,9 +9,45 @@ import UIKit
 
 class LoginFormViewController: UIViewController {
 
+    let signInSegue = "SignInSegue"
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    
+    //MARK: - Sign in implementation
+    
+    @IBAction func signInAction(_ sender: Any) {
+        if checkUserData() {
+            performSegue(withIdentifier: signInSegue, sender: self)
+        } else {
+            showAlertWindow()
+        }
+    }
+    
+    func checkUserData() -> Bool {
+        if let login = loginTextField.text, let password = passwordTextField.text {
+            if login == "" && password == "" {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func showAlertWindow() {
+        let alert = UIAlertController(title: "Error", message: "Wrong user name or password", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Exit to this controller action
+    
+    @IBAction func exitToLoginForm(unwindSegue: UIStoryboardSegue) {
+    }
 }
